2d08bb79a9db192dbff4f506b8b43703dbb36f36
paulinero91 <paulinero91@gmail.com>
2017-08-21 16:21:27 -0400

    put endpoints

--- server.js
+++ server.js
@@ -115,3 +115,32 @@ app.delete('/recipes/:id', (req, res) => {
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
+
+
+//creating put endpoints
+app.put('/recipes/:id', jsonParser, (req, res) => {
+  const requiredFields = ['name', 'id'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  if (req.params.id !== req.body.id) {
+    console.log('here');
+    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating recipe item \`${req.params.id}\``);
+  Recipes.update({
+    id: req.params.id,
+    name: req.body.name,
+    budget: req.body.budget
+  });
+  res.status(204).end();
+});
+
